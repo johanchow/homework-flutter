2b6a60fe8e32b8b4467d156aa412f374e02abcfd
johan <johan>
2025-08-07 08:47:44 +0800

    改变包名

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.homework_flutter"
+    namespace = "com.johanchow.homework_flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13599879"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.homework_flutter"
+        applicationId = "com.johanchow.homework_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
